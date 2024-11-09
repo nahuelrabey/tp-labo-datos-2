91a92389a1bf53c4303d705b289a7b83b54a72fa
Sam-my16 <sivch2012@gmail.com>
2024-11-09 12:10:19 -0300

    Terminé el código de comparación entre números, en este código está definida una función que crear archivos con imágenes de un único número

--- Determinar similitud.py
+++ Determinar similitud.py
@@ -4,7 +4,20 @@ import pandas as pd
 import numpy as np
 from skimage.metrics import structural_similarity as ssim 
 
-#%% Cargar datos
+#%% Preset para gráficos
+# Visualizaciones
+plt.rcParams["figure.figsize"] = (10,8)
+plt.rcParams['font.size'] = 20           
+plt.rcParams['axes.labelsize'] = 20      
+plt.rcParams['axes.titlesize'] = 20      
+plt.rcParams['legend.fontsize'] = 16    
+plt.rcParams['xtick.labelsize'] = 16      
+plt.rcParams['ytick.labelsize'] = 16 
+plt.rcParams['mathtext.fontset'] = 'cm'
+plt.rcParams['font.family'] = 'STIXGeneral'
+
+
+#%% Cargar datos crudos
 
 # un array para las imágenes, otro para las etiquetas (por qué no lo ponen en el mismo array #$%@*)
 data_imgs = np.load('./archivos/mnistc_images.npy')
@@ -17,16 +30,18 @@ print(data_imgs.shape)
 print(data_chrs.shape)
 
 
-#%% Separamos el número de interés
+#%% Función para guardar un archivo que contenga imágenes de un único número
 
 indices = []
 lista_data = []
 lista_chrs = []
 
+#Busco los índices que corresponden las imáganes 
 def separar_numero(x):
     global indices
     indices = [i for i, numero in enumerate(data_chrs) if numero == x]
 
+#Selecciono y guardo las imágenes correspondientes a esos índices 
 def guardar_numero(x):
     global lista_data, lista_chrs
     for i in indices:
@@ -41,12 +56,29 @@ def guardar_numero(x):
     np.save(f"./archivos/numero_{x}.npy", imgset)
     np.save(f"./archivos/numero_{x}_chrs.npy", img_chr)
     
-
-
 separar_numero(9)
 guardar_numero(9)
 
-#%% Determinamos similitud usando ssim
+#%% Función para comparar imágenes usando ssim
+
+def comparar_ssim (x,y): #x : número fijo, y: número a comparar
+    ssim_valores = []
+    for i in range(0,min(len(y), len(x))-1): #así comparamos con el mismo número de imágenes
+        image_1 = x[i,:,:,0]
+        image_2 = y[i+1,:,:,0]
+        valores = ssim(image_1,image_2)
+        ssim_valores.append(valores)
+    return np.mean(ssim_valores)
+
+#%% Función para comparar un número con los demás 
+def obtener_valores_comparacion(x):    
+    valores_comparacion = []
+    for i in range(0,10):
+        y = globals().get(f'imgs{i}')
+        val = comparar_ssim(x, y)
+        valores_comparacion.append(val)
+    return valores_comparacion
+#%% Cargo las imágenes a comparar
 
 imgs0 = np.load('./archivos/numero_0.npy')[:,:,:,np.newaxis]
 imgs1 = np.load('./archivos/numero_1.npy')[:,:,:,np.newaxis]
@@ -59,50 +91,27 @@ imgs7 = np.load('./archivos/numero_7.npy')[:,:,:,np.newaxis]
 imgs8 = np.load('./archivos/numero_8.npy')[:,:,:,np.newaxis]
 imgs9 = np.load('./archivos/numero_9.npy')[:,:,:,np.newaxis]
 
+#%% Comparamos todos los números entre sí y creamos un dataframe
+resultados_totales = {}
 
-ssim_valores = []
-def comparar_ssim (x):
-    for i in range(0,min(len(x), len(imgs7))-1):
-        image_1 = imgs7[i,:,:,0]
-        image_2 = x[i+1,:,:,0]
-        valores = ssim(image_1,image_2)
-        ssim_valores.append(valores)
-    return np.mean(ssim_valores)
-#%%
-valores_comparacion = []
-
-#%%
-valores_comparacion = []
-
-# Loop through files named imgs0, imgs1, ..., imgs9
-for i in range(0,10):
-    x = globals().get(f'imgs{i}')  # Get imgs0, imgs1, ..., imgs9 from global scope
+for i in range(0, 10):
+    x = globals().get(f'imgs{i}')
     if x is not None:
-        comparacion = comparar_ssim(x)
-        valores_comparacion.append(comparacion)
-    else:
-        print(f"Warning: imgs{i} is not defined.") 
+        resultados_totales[f'imgs{i}'] = obtener_valores_comparacion(x)
 
+df_resultados = pd.DataFrame(resultados_totales, index=[f'imgs{i}' for i in range(10)],
+                             columns=[f'imgs{j}' for j in range(10)])
 
-
-    
-
+numeros = np.arange(0,10,1 )
 
 #%%
-n_digit = 80
-image5 = imgs[n_digit,:,:,0]
-image_l5 = chrs[n_digit]
 
+plt.plot(numeros, df_resultados[f'imgs{i}'])
 
-n_digit1 = 68
-image15 = imgs[n_digit1,:,:,0]
-image_l15 = chrs[n_digit1]
-
-
-
-
-
-ssim(image15,image5)
+plt.xticks(np.arange(0,10, 1.0))
+plt.ylabel('Valor promedio de SSIM')
+plt.xlabel('Dígitos')
+plt.title('Similitud entre 7 con demás dígitos')
 
 
 
