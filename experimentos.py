db368fcba9fb1e55d6d6905a8d1d50ee2de7a346
Nahuel Rabey <nahuelrabeywork@gmail.com>
2024-11-12 10:33:12 -0300

    agrego experimento random forest, y guardo los resultados de este experimento

--- experimentos.py
+++ experimentos.py
@@ -1,3 +1,4 @@
+from sklearn.ensemble import RandomForestClassifier
 import helpers as hlps
 import numpy as np
 from sklearn import tree
@@ -6,15 +7,20 @@ from sklearn.model_selection import KFold, train_test_split
 
 FOLDER = "./archivos/"
 
-def crear_path(n: int):
-    res = FOLDER + f"exp_{n}_res.npy"    
-    labels = FOLDER + f"exp_{n}_label.npy"
+def crear_path(name: str):
+    res = FOLDER + f"exp_{name}_res.npy"    
+    labels = FOLDER + f"exp_{name}_label.npy"
+    return res, labels
 
-def cargar_experrimento(n: int): 
+def cargar_resultados(n: int): 
     res, labels = crear_path(n)
     return np.load(res), np.load(labels)
 
-def experimento_1(X, y):
+def experimento_decision_tree():
+    imagenes = hlps.Imagenes()
+    X = imagenes.x_dev
+    y = imagenes.y_dev
+
     alturas = [1,2,3,5,10,20]
     nsplits = 5
     kf = KFold(n_splits=nsplits)
@@ -30,21 +36,47 @@ def experimento_1(X, y):
         kf_y_test = y.iloc[test_index]
 
         for j, altura_max in enumerate(alturas):
-            arbol = tree.DecisionTreeClassifier(max_depth = altura_max) 
+            arbol = tree.DecisionTreeClassifier(max_depth = altura_max, random_state=1) 
             arbol.fit(kf_x_train, kf_y_train)
             pred = arbol.predict(kf_x_test)
             exactitud = accuracy_score(kf_y_test, pred)
             resultados[i,j] = exactitud
 
-    res,labels = crear_path(1)
+    res,labels = crear_path("decision_tree")
     np.save(res, resultados)
     np.save(labels, alturas)
 
-
-if __name__ == "__main__":
+def experimento_random_forest():
     imagenes = hlps.Imagenes()
-    x = imagenes.atributos
-    y = imagenes.clases
-    x_dev, x_eval, y_dev, y_eval = train_test_split(x,y, random_state=1, test_size=0.1)
+    X = imagenes.x_dev
+    y = imagenes.y_dev
 
-    experimento_1(x_dev, y_dev)
+    alturas = [1,2,3,5,10,20]
+    nsplits = 5
+    kf = KFold(n_splits=nsplits)
+    resultados = np.zeros((nsplits, len(alturas)))
+
+    split = kf.split(X)
+    # print(len(split))
+    for i, (train_index, test_index) in enumerate(split):
+        kf_x_train = X.iloc[train_index]
+        kf_x_test = X.iloc[test_index]
+        
+        kf_y_train = y.iloc[train_index]
+        kf_y_test = y.iloc[test_index]
+
+        for j, altura_max in enumerate(alturas):
+            clf = RandomForestClassifier(random_state=1, max_depth=altura_max)
+            clf.fit(kf_x_train, kf_y_train)
+            pred = clf.predict(kf_x_test)
+            exactitud = accuracy_score(kf_y_test, pred)
+            resultados[i,j] = exactitud
+
+    res,labels = crear_path("random_forest")
+    np.save(res, resultados)
+    np.save(labels, alturas)
+    
+if __name__ == "__main__":
+
+    experimento_decision_tree()
+    experimento_random_forest()
