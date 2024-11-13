1ba37eafcc132928cf3c985dbcaa7c0d926fac4a
Sam-my16 <sivch2012@gmail.com>
2024-11-13 12:14:55 -0300

    nada

--- experimentos.py
+++ experimentos.py
@@ -1,7 +1,7 @@
-from sklearn.ensemble import RandomForestClassifier
 import helpers as hlps
 import numpy as np
 from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import KFold, train_test_split
 import matplotlib.pyplot as plt
@@ -30,11 +30,15 @@ def crear_path(n: int):
     labels = FOLDER + f"exp_{n}_label.npy"
     return res, labels
     
-def cargar_experimento(n: int): 
+def cargar_resultados(n: int): 
     res, labels = crear_path(n)
     return np.load(res), np.load(labels)
 
-def experimento_1(X, y):
+def experimento_decision_tree():
+    imagenes = hlps.Imagenes()
+    X = imagenes.x_dev
+    y = imagenes.y_dev
+    
     alturas = [1,2,3,5,10,20]
     nsplits = 5
     kf = KFold(n_splits=nsplits)
@@ -55,20 +59,44 @@ def experimento_1(X, y):
             exactitud = accuracy_score(kf_y_test, pred)
             resultados[i,j] = exactitud
             
-    res,labels = crear_path(1)
+    res,labels = crear_path("decision_tree")
     np.save(res, resultados)
     np.save(labels, alturas)
     
-if __name__ == "__main__":
+def experimento_random_forest():
     imagenes = hlps.Imagenes()
-    mask = imagenes.clases.isin(digitos_especificos)  # Filtramos las imágenes para obtener solo de los dígitos específicos
-    x = imagenes.atributos[mask]
-    y = imagenes.clases[mask]
-    x_dev, x_eval, y_dev, y_eval = train_test_split(x,y, random_state=1, test_size=0.1)
-    experimento_1(x_dev, y_dev)
-    
+    X = imagenes.x_dev
+    y = imagenes.y_dev
+    alturas = [1,2,3,5,10,20]
+    nsplits = 5
+    kf = KFold(n_splits=nsplits)
+    resultados = np.zeros((nsplits, len(alturas)))
+    split = kf.split(X)
+    # print(len(split))
+    for i, (train_index, test_index) in enumerate(split):
+        kf_x_train = X.iloc[train_index]
+        kf_x_test = X.iloc[test_index]
+        
+        kf_y_train = y.iloc[train_index]
+        kf_y_test = y.iloc[test_index]
+        for j, altura_max in enumerate(alturas):
+            clf = RandomForestClassifier(random_state=1, max_depth=altura_max)
+            clf.fit(kf_x_train, kf_y_train)
+            pred = clf.predict(kf_x_test)
+            exactitud = accuracy_score(kf_y_test, pred)
+            resultados[i,j] = exactitud
+
+    res,labels = crear_path("random_forest")
+    np.save(res, resultados)
+    np.save(labels, alturas)
+
+#%%
+if __name__ == "__main__":
+    experimento_decision_tree()
+    experimento_random_forest()
+
 #%% Graficamos la precisión promedio en función de la profundidad del árbol
-res, labels = cargar_experimento(1)
+res, labels = cargar_resultados("decision_tree")
 exactitud_promedio = res.mean(axis=0)
 
 plt.plot(labels, exactitud_promedio, marker='o', linestyle='-')
